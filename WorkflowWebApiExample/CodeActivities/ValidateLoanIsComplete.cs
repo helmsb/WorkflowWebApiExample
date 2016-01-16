683b4ae6dd1e0fa94064388d1ade410bafb8fece
bhelms <blake@blakehelms.net>
2016-01-16 13:31:24 -0600

    Added sample code activity with "RequiredAttribute"

---
+++ WorkflowWebApiExample/CodeActivities/ValidateLoanIsComplete.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+using WorkflowWebApiExample.Models;
+
+namespace WorkflowWebApiExample.CodeActivities
+{
+
+    public sealed class ValidateLoanIsComplete : CodeActivity
+    { 
+        [RequiredArgument]
+        public InArgument<Loan> Loan { get; set; }
+
+        [RequiredArgument]
+        public OutArgument<bool> Valid { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            context.SetValue(Valid, false);
+
+            var loan = context.GetValue(this.Loan);
+
+            if(loan.CreditRating > 0)
+            {
+                context.SetValue(Valid, true);
+            }
+
+            if(loan.DownPaymentAmount > 0)
+            {
+                context.SetValue(Valid, true);
+            }
+
+            if(loan.LoanAmount > 0)
+            {
+                context.SetValue(Valid, true);
+            }
+
+        }
+    }
+}
